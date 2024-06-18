58e57a1caefff1dbbcfb22cf01686accb6ad735b
Souzaa <zinfegr@gmail.com>
2024-06-18 04:30:02 -0300

    Pointer + Estrutura

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import { useEffect, useState } from 'react'
 
-function App() {
+interface Position {
+  y: number;
+  x: number;
+}
+
+const App: React.FC = () => {
+  const [position, setPosition] = useState<Position>({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setPosition({ x: e.clientX -5, y: e.clientY -5 });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 
   return (
     <div>
+      <div className='ball' style={{ transform: `translate(${position.x}px, ${position.y}px)` }}></div>
       <Header />
       <Hero />
       <hr />
